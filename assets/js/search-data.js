aeb3dda83193db09a2da46ba3c6624603b488c59
povashraful <ashrafulhossainwork@gmail.com>
2025-08-15 10:19:21 +0000

    Deploying to gh-pages from @ povashraful/povashraful.github.io@f3d31170959529cf32b6d8f4fb951b2348311874 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,9 +430,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-country-no-1",
-          title: 'Country No. 1',
-          description: "with background image",
+          section: "News",},{id: "projects-country-no-1-singapore",
+          title: 'Country No. 1 - Singapore',
+          description: "Singapore",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
