45e4bfbeafa77724f2a425dbd56b1dd8d16ec876
povashraful <ashrafulhossainwork@gmail.com>
2025-08-15 09:13:58 +0000

    Deploying to gh-pages from @ povashraful/povashraful.github.io@3e3efe6a3f0ef2f6194e852852314110a4ed118c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -441,7 +441,7 @@ ninja.data = [{
           title: 'Country No. 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_country/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
