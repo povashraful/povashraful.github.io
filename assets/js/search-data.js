8a91bb8ad188810b12801c6137df5cd48eda7987
povashraful <ashrafulhossainwork@gmail.com>
2025-11-14 16:43:08 +0000

    Deploying to gh-pages from @ povashraful/povashraful.github.io@502582971c3d5848c5bf287759578e132cdef371 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -480,7 +480,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%73%68%72%61%66%75%6C%68%6F%73%73%61%69%6E%77%6F%72%6B@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
